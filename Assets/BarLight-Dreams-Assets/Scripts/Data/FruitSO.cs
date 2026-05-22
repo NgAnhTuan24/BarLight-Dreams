fe074c16350dfecb6d79df36119362c692e0dd75
Halns24 <138025120+NgAnhTuan24@users.noreply.github.com>
2026-05-22 10:07:32 +0700

    Revert "Feature/drink system"

--- Assets/BarLight-Dreams-Assets/Scripts/Data/FruitSO.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Fruit Name", menuName = "Bar/Fruit")]
-public class FruitSO : ScriptableObject
-{
-    public IngredientType fruitName;
-    public Sprite fruitIcon;
-}
--- Assets/BarLight-Dreams-Assets/Scripts/Data/WineSO.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Wine Name", menuName = "Bar/Wine")]
-public class WineSO : ScriptableObject
-{
-    public IngredientType wineName;
-    public Sprite wineIcon;
-}
