f1f5efaad53737d19564a18443282353ad5b5cb8
NgAnhTuan24 <luckytme2004@gmail.com>
2026-06-09 10:06:50 +0700

    feat: add fade and scale transitions for UI panels

--- Assets/BarLight-Dreams-Assets/Scripts/Mics/UIPanelSwitcher.cs
+++ Assets/BarLight-Dreams-Assets/Scripts/Mics/UIPanelSwitcher.cs
@@ -1,24 +1,73 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class UIPanelSwitcher : MonoBehaviour
 {
     [SerializeField] private GameObject panelToShow;
+    [SerializeField] private CanvasGroup panelCanvasGroup;
+
     [SerializeField] private GameObject[] objectsToHide;
     [SerializeField] private GameObject[] objectsToShowWhenClose;
+
+    [SerializeField] private float duration = 0.25f;
 
     public void Open()
     {
         panelToShow.SetActive(true);
 
+        panelCanvasGroup.alpha = 0;
+        panelToShow.transform.localScale = Vector3.one * 0.8f;
+
+        panelCanvasGroup.DOFade(1, duration);
+        panelToShow.transform.DOScale(1f, duration).SetEase(Ease.OutBack);
+
         foreach (var obj in objectsToHide)
-            obj.SetActive(false);
+        {
+            HideEffect(obj);
+        }
     }
 
     public void Close()
     {
-        panelToShow.SetActive(false);
+        panelCanvasGroup.DOFade(0, duration);
+
+        panelToShow.transform
+            .DOScale(0.8f, duration)
+            .SetEase(Ease.InBack)
+            .OnComplete(() =>
+            {
+                panelToShow.SetActive(false);
+            });
 
         foreach (var obj in objectsToShowWhenClose)
-            obj.SetActive(true);
+        {
+            ShowEffect(obj);
+        }
+    }
+
+    private void HideEffect(GameObject obj)
+    {
+        CanvasGroup cg = obj.GetComponent<CanvasGroup>();
+
+        if (cg == null) cg = obj.AddComponent<CanvasGroup>();
+
+        cg.DOFade(0, duration).OnComplete(() => obj.SetActive(false));
+
+        obj.transform.DOScale(0.8f, duration);
+    }
+
+    private void ShowEffect(GameObject obj)
+    {
+        CanvasGroup cg = obj.GetComponent<CanvasGroup>();
+
+        if (cg == null) cg = obj.AddComponent<CanvasGroup>();
+
+        obj.SetActive(true);
+
+        cg.alpha = 0;
+        obj.transform.localScale = Vector3.one * 0.8f;
+
+        cg.DOFade(1, duration);
+        obj.transform.DOScale(1f, duration).SetEase(Ease.OutBack);
     }
 }
