87a8d35d1f1d40e5925ce8ffe5daa587b50e302c
NgAnhTuan24 <luckytme2004@gmail.com>
2026-06-16 20:49:03 +0700

    add text version in scene main menu

--- Assets/BarLight-Dreams-Assets/Scripts/Mics/MenuGame.cs
+++ Assets/BarLight-Dreams-Assets/Scripts/Mics/MenuGame.cs
@@ -1,15 +1,19 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuGame : MonoBehaviour
 {
     [SerializeField] private string sceneName = "GamePlay";
+    [SerializeField] private TMP_Text versionText;
 
     [SerializeField] private AudioClip musicGame;
 
     private void Start()
     {
         Time.timeScale = 1;
+
+        versionText.text = $"DEMO v{Application.version}";
 
         AudioManager.instance.PlayMusic(musicGame);
     }
